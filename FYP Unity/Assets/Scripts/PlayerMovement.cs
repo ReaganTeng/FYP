5580377745bc32aa44373140aae911ee6e8c10e1
jyporky <babyanchoo@gmail.com>
2023-03-10 09:39:46 +0800

    New Project

---
+++ FYP Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody playerRB;
+    [SerializeField] Transform orientation;
+    [SerializeField] float PlayerSpeed;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRB.AddForce((orientation.forward * verticalInput + orientation.right * horizontalInput) * PlayerSpeed);
+
+    }
+}
