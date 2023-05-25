aa2cebbde1a09280bbc42a27c203dba877720d00
EvanQP <69576334+EvanQP@users.noreply.github.com>
2023-05-26 05:03:41 +0800

    more changes and final vn push

--- FYP Unity/Assets/VN Dialogue Test/Demo/Scripts/TestMessage.cs
+++ FYP Unity/Assets/VN Dialogue Test/Demo/Scripts/TestMessage.cs
@@ -65,25 +65,25 @@ public class TestMessage : MonoBehaviour
                 lvl1vn();
                 break;
             case SceneName.L2VN:
-                lvl1vn();
+                lvl2vn();
                 break;
             case SceneName.L3VN:
-                lvl1vn();
+                lvl3vn();
                 break;
             case SceneName.L4VN:
-                lvl1vn();
+                lvl4vn();
                 break;
             case SceneName.L5VN:
-                lvl1vn();
+                lvl5vn();
                 break;
             case SceneName.L6VN:
-                lvl1vn();
+                lvl6vn();
                 break;
             case SceneName.L7VN:
-                lvl1vn();
+                lvl7vn();
                 break;
             case SceneName.L8VN:
-                lvl1vn();
+                lvl8vn();
                 break;
             case SceneName.L8VN_Win:
                 lvl1vn();
@@ -336,20 +336,226 @@ public class TestMessage : MonoBehaviour
 
         dialogTexts.Add(new DialogData("/emote:nosprite/ So they will probably take awhile more to kill than the other 2 ingredients but the dishes made from it is worth alot points, so its very well worth the effort.", "HeadChef"));
 
-        dialogTexts.Add(new DialogData("/emote:nosprite/ Well, I think that about covers all the ingredients that are currently availible to use at the moment, and it almost opening time", "HeadChef", () => background.sprite = imageList[1]));
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Well, I think that about covers all the ingredients that are currently availible to use at the moment, and its almost opening time anyways", "HeadChef", () => background.sprite = imageList[1]));
 
         dialogTexts.Add(new DialogData("/emote:nosprite/ Hopefully those tips will aid you in bringing glory to Isarata's name", "HeadChef", () => background.sprite = imageList[12]));
 
         dialogTexts.Add(new DialogData("/emote:worried/ That's alot to take in, but thanks anyways. You seem to be in an awfully good mood today", "Ramses", () => background.sprite = imageList[1]));
 
-        dialogTexts.Add(new DialogData("/emote:nosprite/ Well, I want this restuarant to succeed as much as you want to go home human. So quickly get to work station, the first customers are about to order up", "HeadChef", () => SceneManager.LoadScene("Game Scene")));
-
-        
-
-
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Well, I want this restuarant to succeed as much as you want to go home human. So quickly get to work station, the first customers are about to order up!", "HeadChef", () => SceneManager.LoadScene("Game Scene")));
 
 
         DialogManager.Show(dialogTexts);
     }
 
+    private void lvl2vn()
+    {
+
+        LM.DaySelected = 2;
+
+        var dialogTexts = new List<DialogData>();
+
+
+
+        dialogTexts.Add(new DialogData("Time to go to the head chef's briefing", "Ramses", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/Good morning Ramses, yesterday's opening day went smoothly thanks to you.", "HeadChef", () => background.sprite = imageList[12]));
+
+        dialogTexts.Add(new DialogData("Yesterday was tough but still managable I guess", "Ramses", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/Keep up with the good work, its only going to get tougher from here on out so prepare yourself.", "HeadChef", () => background.sprite = imageList[11]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Today's recipes will require you to prepare some Saamono, an all time favourite ingredient among all aliens both young and old", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ The Saamono we have here are the fresh water type so they are relatively easy to prepare", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Recipes involving Saamono will usually also require varying amounts of premade ingredients as well so you could even prepare for them in advance if you wanted to", "HeadChef", () => background.sprite = imageList[6]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Next, we have Eggyoo. They will charge at you similar to Aapolos you've prepared before except these guys are one tough shell to crack", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ So its advised to try and get multiple ingredients from them to save you time spent walking over to them at the other corner of the kitchen", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ There are benefits to going out of your way though since they can help you build your combo and fervor meter up easily.", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Just a re-fresher in case you have forgotten, your fervor will grant a bonus star to any final dish you make while it is active so be sure to build up your combos", "HeadChef", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ And that just about wraps up todays briefing. Are there any questions?", "HeadChef", () => background.sprite = imageList[12]));
+
+        dialogTexts.Add(new DialogData("Are the dishes we served yesterday still in today's menu?", "Ramses", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Yes, you can expect to see repeat dishes from previous days so all your cooking knowledge will be cumilative", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Oh! Here comes the first few customers, lets get cooking.", "HeadChef", () => SceneManager.LoadScene("Game Scene")));
+
+
+        DialogManager.Show(dialogTexts);
+    }
+
+    private void lvl3vn()
+    {
+
+        LM.DaySelected = 3;
+
+        var dialogTexts = new List<DialogData>();
+
+        dialogTexts.Add(new DialogData("Time to go to the head chef's briefing", "Ramses", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/Morning Ramses, I see you are very quickly adapting to our kitchen already", "HeadChef", () => background.sprite = imageList[12]));
+
+        dialogTexts.Add(new DialogData("Well, theres a saying back on earth that The perfect chef is flexible and adapts to the needs and requests of clients always", "Ramses", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/Well said. I think I'll also start incoporating that motto into my junior chef training school. Wait, we are getting sidetracked...", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Anyways, we are reaching the point where all new ingredients introduced from now on are all fairly hard to prepare without sufficient upgrades.", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Make sure you are always using up any cosmic credit you have on you since Respec-ing your upgrades is always free and can be done anytime in the shop", "HeadChef", () => background.sprite = imageList[8]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Firstly, Kaaroots will charge and fire lasers at you constantly so it will probably take longer than usual to prepare them", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Keep an eye out for their laser indicator and dodge at the right timing to avoid getting hit", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Thankfully they can be interupted when ever they take damage while charging up their lasers so use this knowledge as you see fit.", "HeadChef", () => background.sprite = imageList[9]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Then we have the Pinaachoo's that fires concentrated toxic balls in bursts of 3 at you.", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ You could try to dash away or through their projectiles if you are elusive enough", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ But my advise is to use the large room to manipulate their behaviours to your advantage", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ And that just about covers the new ingredients introduced today. Good luck with today's orders, they can be quite difficult at first", "HeadChef", () => background.sprite = imageList[12]));
+
+        dialogTexts.Add(new DialogData("/emote:worried/ What if I get stuck and can't complete today's orders in time", "Ramses", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Have you tried going back to a previous day for a higher ranking grade? You can earn more cosmic credits that way then come back to today when you get more upgrades", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Ah crap, no time to explain the quantam physics on how that works now. People are already queing up to eat, Go Go GO!", "HeadChef", () => SceneManager.LoadScene("Game Scene")));
+
+
+        DialogManager.Show(dialogTexts);
+
+
+    }
+
+    private void lvl4vn()
+    {
+
+        LM.DaySelected = 4;
+
+        var dialogTexts = new List<DialogData>();
+
+        dialogTexts.Add(new DialogData("Time to go to the head chef's briefing", "Ramses", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/Hey Ramses, you're here early today.. Are you that eager to cook?", "HeadChef", () => background.sprite = imageList[12]));
+
+        dialogTexts.Add(new DialogData("I don't know what came over me this morning, I just felt like I'm way more energetic in the mornings lately", "Ramses", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/Its probably got something to do with all the upgrades you have by this point. They not only improve your cooking skills but also your overall health", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Once you get even more upgrades you will start to feel like a super human in the kitchen mostly but still a great feeling nonetheless.. Onto todays briefing!", "HeadChef", () => background.sprite = imageList[7]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ These Jiikens might seem cute and harmless, but they do not go down easily at all. Killing just 1 of them is already a herculean task", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ If I were you I would round them all up together before every heavy attack and try to hit as many as I can with the same attacks", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ This method also builds up a combo really fast so by the time you are done with them you should have your fervor ready to cook", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Each Jiiken also attacks really fast so watch out for that.", "HeadChef", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Finally, we've come to our last enemy, the Moo Moos and its also the beefiest one yet.", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ These guys here actually managed to form their own cult and they gained a new power to shoot out demonic spells recently", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Even I don't dare go them these days but we don't have a choice if we want tasty beef dishes", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ With that I have covered all the ingredients our restaurant Isarata will be using to impress our Cosmic Critics arriving any day now.", "HeadChef", () => background.sprite = imageList[12]));
+
+        dialogTexts.Add(new DialogData("/emote:worried/ Who are the Cosmic Critics anyways? And why are you so hell bent on getting a review from them", "Ramses", () => background.sprite = imageList[1]));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Nobody knows who they really are. The Cosmic Critics are a mysterious society that has the power to make any restaurant extremely successful after they leave a 5 Star Boogle Review post behind", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:nosprite/ Our objective is just to obtain that 5 star review at all costs... Anyways enough chit chat, these orders won't serve themselves! Get a move on!", "HeadChef", () => SceneManager.LoadScene("Game Scene")));
+
+
+        DialogManager.Show(dialogTexts);
+    }
+
+    private void lvl5vn()
+    {
+
+        LM.DaySelected = 5;
+
+        var dialogTexts = new List<DialogData>();
+
+        dialogTexts.Add(new DialogData("Theres no head chef briefing for today, I should just head to the kitchen and start todays work", "Ramses", () => SceneManager.LoadScene("Game Scene")));
+
+
+        DialogManager.Show(dialogTexts);
+    }
+
+    private void lvl6vn()
+    {
+
+        LM.DaySelected = 6;
+
+        var dialogTexts = new List<DialogData>();
+
+        dialogTexts.Add(new DialogData("Theres no head chef briefing for today, I should just head to the kitchen and start todays work", "Ramses", () => SceneManager.LoadScene("Game Scene")));
+
+
+        DialogManager.Show(dialogTexts);
+    }
+
+    private void lvl7vn()
+    {
+
+        LM.DaySelected = 7;
+
+        var dialogTexts = new List<DialogData>();
+
+        dialogTexts.Add(new DialogData("Theres no head chef briefing for today, I should just head to the kitchen and start todays work", "Ramses", () => SceneManager.LoadScene("Game Scene")));
+
+
+        DialogManager.Show(dialogTexts);
+    }
+
+    private void lvl8vn()
+    {
+
+        LM.DaySelected = 8;
+
+        var dialogTexts = new List<DialogData>();
+
+        dialogTexts.Add(new DialogData("/emote:worried/ *This is it Ramses, today is finally the day that decides if I get to go home in 1 piece or not.*.", "Ramses"));
+
+        dialogTexts.Add(new DialogData("Time to go to the head chef's final briefing", "Ramses"));
+
+        dialogTexts.Add(new DialogData("I must say Ramses, your cooking reputation on earth really precedes you. I was actually skeptical at first but you blew our expectations out of the water", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("As Isarata stands now we already have a steady flow of customers everyday thanks to your hard work but Isarata can become something greater.", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("Once we secure that 5 Star Boogle review we will definitely be able to expand and become a popular chain store across the galaxy.", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:angry/Hey. What in it for me then? I’ve been working really hard this past week for your success but what do i get out of it?", "Ramses"));
+
+        dialogTexts.Add(new DialogData("Well, you get to go home", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:angry/…", "Ramses"));
+
+        dialogTexts.Add(new DialogData("And you may leave with all of the upgrades you have earned through out your time here at Isarata", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("Wow, thats actually a pretty good deal! I’ve kinda grown used to these abilities so it would be a shame to part with them", "Ramses"));
+
+        dialogTexts.Add(new DialogData("Yes, its a really good deal indeed. But only if you pass their standards. Speaking of their standards we estimate that you will need at least a minimum score of 250 to meet their expectations.", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("/emote:worried/250 is alot… What about they types of dishes they might order? Do they have a favourite type of food or such?", "Ramses"));
+
+        dialogTexts.Add(new DialogData("Unfortunately we have no clue. So just be prepared for anything, cause his orders are always unpredictable. Its almost as if its randomized or something.", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("They should be here any minute now, I suggest you stand prepared at your work station in advance. All the best Ramses.", "HeadChef"));
+
+        dialogTexts.Add(new DialogData("Ok. Its Time /wait:1/To /wait:1/Cook.", "Ramses", () => SceneManager.LoadScene("Game Scene")));
+
+
+        DialogManager.Show(dialogTexts);
+    }
 }
